d24e7fb8cf272de19429ab33d699b632aec0be52
Keido Sumi <delta0726@iuc-net.ne.jp>
2022-10-21 06:47:02 +0900

    Update 5-5

--- book/empirical_accounting/5-5_統計的推論入門.R
+++ book/empirical_accounting/5-5_統計的推論入門.R
@@ -25,18 +25,20 @@
 
 # ライブラリ
 library(tidyverse)
+library(tidyquant)
 library(magrittr)
 
 
 # データロード
-stock_data <- read_csv("data/ch05_stock_data.csv")
+stock_data_raw <- read_csv("data/ch05_stock_data.csv")
 
 
 # 1 データ加工 ----------------------------------------------------------------
 
 # 加工済データの作成
+# --- 以降で必要なデータのみ抽出
 stock_data <-
-  stock_data %>%
+  stock_data_raw %>%
     group_by(firm_ID) %>%
     mutate(lagged_stock_price = lag(stock_price)) %>%
     ungroup() %>%
@@ -44,11 +46,15 @@ stock_data <-
            stock_price_adj = (stock_price + DPS) * adjustment_coefficient,
            R = (stock_price_adj - lagged_stock_price) / lagged_stock_price,
            Re = R - R_F) %>%
-    select(year, month, month_ID, firm_ID, stock_price, stock_price_adj,
-           lagged_stock_price, adjustment_coefficient, DPS, R, R_F, Re, ME)
+    select(year, month, month_ID, firm_ID, R, R_F, Re)
 
 
 # 2 リターンデータに対する仮定 ---------------------------------------------------
+
+# ＜ポイント＞
+# - 月次リターンは独立であり系列相関は持たない
+#   --- ボラティリティは系列相関を持つが、リターンのディレクションに系列相関はない
+
 
 # 時系列プロット作成
 # --- firm_IDが1の企業のデータのみ抽出
@@ -59,7 +65,7 @@ stock_data %>%
   geom_bar(stat = "identity") +
   labs(x = "Month ID", y = "Firm 1's Monthly Excess Return") +
   scale_x_continuous(expand = c(0.01, 0)) +
-  theme_classic()
+  theme_tq()
 
 # ヒストグラム作成
 stock_data %>%
@@ -68,14 +74,23 @@ stock_data %>%
   geom_histogram() +
   labs(x = "Firm 1's Monthly Excess Return", y = "Count") +
   scale_y_continuous(expand = c(0, 0)) +
-  theme_classic()
+  theme_tq()
 
 
 # 3 推定量と推定値の違い --------------------------------------------------------
 
+# ＜ポイント＞
+# - 観測されたデータの平均値は｢推定量｣又は｢標本平均｣と呼ばれる
+#   --- 仮に無限個のデータを観測できたら｢標本平均｣は｢真の期待値｣に収束する（大数の法則）
+#   --- 現実には有限個のサンプルしか観測できないため、ランダムサンプリングによる誤差の影響を受ける
+
+# ＜推定量と推定値の違い＞
+# - 推定量；特定の観測データに基づく実現値（1つのリターンサンプルから計算した平均値）
+# - 推定量；観測データの関数として表される確率変数（様々なリターンサンプルから計算した平均値の確率分布）
+
+
 # 平均値の推定値
-# --- 母集団の期待値は観察不可能なので、観察されたデータの平均値は推定値に過ぎない
-# --- 大数の法則によると、仮に無限個のデータが観測できた場合、標本平均は真の期待値に収束する
+# --- 推定量
 stock_data %>%
   filter(firm_ID == 1) %>%
   use_series(Re) %>%
@@ -83,6 +98,11 @@ stock_data %>%
 
 
 # 4 月次超過リターンのt値の計算 ---------------------------------------------------
+
+# ＜ポイント＞
+# - 1つの平均値から有意に｢r>0｣であることを示すことはできない
+#   --- t値を使うとサンプルの標準誤差を考慮するので論理性が高まる
+
 
 # 超過リターン系列の取得
 Re_firm_ID_1 <-
@@ -98,7 +118,7 @@ mu0 <- 0
 n <- Re_firm_ID_1 %>% length()
 
 # 検定統計量の計算
-# --- t値
+# --- t値（推定値が期待値からどの程度離れているかの統計量）
 t_value <- (mean(Re_firm_ID_1) - mu0) / sqrt(var(Re_firm_ID_1) / n)
 
 
