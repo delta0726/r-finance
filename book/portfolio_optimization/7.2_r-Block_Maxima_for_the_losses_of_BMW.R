8c4f2cb6279c1960f7e8f0ce8837f59f8edbfe43
Keido Sumi <delta0726@iuc-net.ne.jp>
2022-11-13 09:02:37 +0900

    Update 7.2

--- book/portfolio_optimization/7.2_r-Block_Maxima_for_the_losses_of_BMW.R
+++ book/portfolio_optimization/7.2_r-Block_Maxima_for_the_losses_of_BMW.R
@@ -1,22 +1,31 @@
 # ***********************************************************************************************
 # Title   : Financial Risk Modeling and Portfolio Optimization with R
 # Chapter : 7 Extreme value theory
-# Theme   : 2 r-block maxima for BMW losses
-# Date    : 2022/11/02
+# Theme   : 4.3 r-block maxima for BMW
+# Date    : 2022/11/13
 # Page    : P107 - P110
 # URL     : https://www.pfaffikus.de/rpacks/
 # ***********************************************************************************************
 
 
 # ＜概要＞
-# -
+# - Block maximaでは最大値のみを抽出したが、問題となるのはサンプル数
+#   --- 特にファイナンス系のデータの場合、数十年単位で最大値を確保できないことが多い
+# - サンプル数への対処法はr-block maximaモデルを検討する
+#   --- r-block maximaは上からr個の値を抽出します。
+
+
+# ＜参考＞
+# - Rを使って極値理論をファイナンスへ応用してみた（その２）
+# https://qiita.com/hrkz_szk/items/2c966aab9342f61a5b59
 
 
 # ＜目次＞
 # 0 準備
-# 1 最大損失データの抽出
-# 2 最大損失のプロット
-# 3 フィッティングとモデル診断
+# 1 データ準備
+# 2 最大損失データの抽出
+# 3 上位2つの損失をプロット
+# 4 フィッティングとモデル診断
 
 
 # 0 準備 ----------------------------------------------------------------------
@@ -36,26 +45,51 @@ data(bmw)
 bmw %>% class()
 bmw %>% as.data.frame() %>% head()
 
+
+# 1 データ準備 --------------------------------------------------------------------
+
+# 日付データ
+BmwDates <-
+  bmw %>%
+    attr("times") %>%
+    as.POSIXct() %>%
+    format( "%Y-%m-%d") %>%
+    as.character()
+
+# リターン作成
+# --- パーセント表示
+BmwRet <-
+  bmw %>%
+    timeSeries(charvec = BmwDates) %>%
+    multiply_by(100)
+
 # プロット
-BmwDates <- as.character(format(as.POSIXct(attr(bmw, "times")), "%Y-%m-%d"))
-BmwRet <- timeSeries(bmw * 100, charvec = BmwDates)
 BmwRet %>% plot(ylab="Log-return of BMW")
-
-
-# 1 最大損失データの抽出 ---------------------------------------------------------------
 
 # データ変換
 # --- 小数をパーセントに変換して正負を逆転（損失を正の値として扱う）
 BmwLoss <- bmw * 100 * -1.0
 
+# 属性追加
+attr(BmwLoss, "years") <- BmwLoss %>% attr( "time") %>% format("%Y")
+BmwLoss %>% glimpse()
+
+
+# 2 最大損失データの抽出 ---------------------------------------------------------------
+
+# ＜ポイント＞
+# - 年ごとの損失値の上位2つを抽出してデータセットとしてまとめる
+
+
 # パーツ作成
+# --- 年データからインデックス作成
 Yearu <- BmwLoss %>% attr("time") %>% format( "%Y") %>% unique()
 idx <- 1:length(Yearu)
 
 # 関数定義
 # --- 最大損失の上位2つを抽出
 test_f <- function(x){
-  BmwLoss[attr(BmwLoss, "years") == Yearu[x]] %>%
+  BmwLoss[attr(BmwLoss, "year") == Yearu[x]] %>%
     sort(decreasing = TRUE) %>%
     head(2)
 }
@@ -68,8 +102,11 @@ BmwOrder <-
     set_colnames(c("r1", "r2")) %>%
     set_rownames(Yearu)
 
+# 確認
+BmwOrder %>% print()
 
-# 2 最大損失のプロット -----------------------------------------------------------------
+
+# 3 上位2つの損失をプロット -----------------------------------------------------------
 
 Yearu %>%
   plot(BmwOrder[, 1], col = "black", ylim = range(BmwOrder),
@@ -80,13 +117,20 @@ Yearu %>%
   points(BmwOrder[, 2], col = "grey", pch = 23, bg = "grey")
 
 
-# 3 フィッティングとモデル診断 ---------------------------------------------------------
+# 4 フィッティングとモデル診断 ---------------------------------------------------------
 
 # フィッティング
-BmwOrderFit <- BmwOrder %>% rlarg.fit()
+# --- convのゼロはうまく推定されたことを示している
+BmwOrderFit <- BmwOrder %>% ismev::rlarg.fit()
 
 # 確認
 BmwOrderFit %>% print()
+
+# 信頼区間
+lower <- BmwOrderFit$mle - 1.96 * BmwOrderFit$se
+upper <- BmwOrderFit$mle + 1.96 * BmwOrderFit$se
+data.frame(lower = lower, upper = upper, row.names = c("mu", "sigma", "xi"))
+
 
 # モデル診断
 BmwOrderFit %>% rlarg.diag()
